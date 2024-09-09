32bb4d15e212879a0b7e00bd59832a2b3b278efb
Sam Hutcher <sam.hut9000@vagaro.com>
2024-09-09 10:05:41 -0700

    Initial Commit With Place Holders

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
 // src/App.tsx
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import BoxRenderer from './components/BoxRenderer';
 
-import ArticlePage from './components/ArticlePage';
-import Home from './components/Home';
 
 const App: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      {/* Dynamic slug route */}
-      <Route path="/article/:slug" element={<ArticlePage />} />
+      <Route path="/" element={<BoxRenderer />} />
     </Routes>
   );
 };
---
+++ src/components/BoxRenderer.tsx
@@ -0,0 +1,16 @@
+
+
+const API_URL = 'https://reactinterviewwebapp-grazc9gvejc7g2au.eastus-01.azurewebsites.net/';
+
+export default function BoxRenderer() {
+
+
+/*Replace Add your code here */
+  return (
+    < div style={{width:"100vw",textAlign:"center"}}>
+        <h1>Your Code Here</h1>
+        <p>Use the API below to render a series of boxes</p>
+        <a href={API_URL}>API URL</a>
+    </div>
+  );
+}
